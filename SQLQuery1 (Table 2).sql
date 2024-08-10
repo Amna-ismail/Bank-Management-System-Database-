c0ac5440afa33061bdca631bfca34d87ec1cf656
Amna <110226508+Amna-ismail@users.noreply.github.com>
2024-08-10 23:27:43 +0500

    Add files via upload

---
+++ SQLQuery1 (Table 2).sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_2] 
+(
+[ID]  [varchar] (max) Not NULL,
+[Name]  [varchar] (max) NULL,
+[Address]  [varchar](max) NULL,
+)
---
+++ ~AutoRecover.SQLQuery1 (Table 2).sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE [dbo].[Table_1] 
+(
+[ID]  [varchar] (max) Not NULL,
+[Name]  [varchar] (max) Not NULL,
+[Address]  [varchar](max) Not NULL
+);
